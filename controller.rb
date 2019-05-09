a5bcf52068271dc858d62f4cf97d7719e661dba9
itggot-christoffer-polndorfer-oresten <christoffer.polndorferoresten@itggot.se>
2019-05-09 10:59:18 +0200

    Beginning of documentation

--- controller.rb
+++ controller.rb
@@ -7,12 +7,22 @@ require'BCrypt'
 require 'rack-flash'
 require_relative 'modell.rb'
 enable :sessions
+include Meme_vault 
 
 
+# Display Landing Page and a login form
+#
 get('/') do 
     slim(:login)
 end 
 
+# Attepmts login and updates the session
+# 
+# @param [String] username, The username
+# @param [String] password, The password
+#
+# @see Meme_vault#login_verification
+# @see Meme_vault#select_session_id
 post('/login') do 
     if login_verification(params["Username"], params["Password"]) == true
         session[:id] = select_session_id(params["Username"])[0]["UserId"]
@@ -25,18 +35,29 @@ post('/login') do
     end 
 end 
 
+# Displays a create vault form
+#
 get('/create_vault') do 
     slim(:create_vault)
 end 
 
+# Creates a new vault and redirects to '/vault'
+#
+# @param [String] username, The vaults user
+# @param [String] password, The users password
+# 
+# @see Meme_vault#connect_to_db
+# @see Meme_vault#encrypt_password
 post('/creating_vault') do 
     db = connect_to_database()
-    # Kryptera lösen & lägg till i databas
     encrypt_password(params["Username"], params["Password"])
     session[:name] = params["Username"]
     redirect('/vault')
 end 
 
+# Displays all memes
+#
+# @see Meme_vault#memes
 get('/vault') do 
     if session[:logged_in?] == true 
         meme = memes()
@@ -47,11 +68,16 @@ get('/vault') do
     end 
 end 
 
+# Logouts and destroys the session
+#
 post('/locking_vault') do 
     session.destroy
     redirect('/')
 end 
 
+# Displays a form for uploading memes
+#
+# @see Meme_vault#get_tags
 get('/upload_meme') do 
     if session[:logged_in?] == true 
         tags = get_tags()
@@ -61,6 +87,12 @@ get('/upload_meme') do
     end 
 end 
 
+# Attempts to add a tag and redirects to '/upload_meme'
+#
+# @param [String] tag, The new tag
+# 
+# @see Meme_vault#check_tag
+# @see Meme_vault#add_tag
 post('/add_tag') do 
     if params["new_tag"].length > 0
         if check_tag(params["new_tag"]) != true
@@ -76,6 +108,15 @@ post('/add_tag') do
     end 
 end 
 
+# Uploads a new meme and redirects to '/vault'
+#
+# @param [File] img, The image file
+# @param [String] imgname, The image name
+# @param (String) memetag1, The first tag
+# @param (String) memetag2, The second tag
+# @param (String) memetag3, The thrid tag
+#
+# @see Meme_vault#upload_meme
 post('/uploading_meme') do 
     img = params[:img][:tempfile]
     imgname = params[:img][:filename]
@@ -88,12 +129,32 @@ post('/uploading_meme') do
     redirect('/vault')
 end 
 
+# Deletes an existing meme and redirects to '/vault'
+#
+# @param [Integer] memeid, Meme Id
+#
+# @see Meme_vault#delete_meme
 post('/delete_meme/:memeid') do
     delete_meme(params["memeid"])
     redirect('/vault')
 
 end 
 
+# Displays search result based on paramaters
+#
+get('/vault_search') do 
+    if session[:logged_in?] == true
+        sessionid = session[:id] 
+        slim(:vault_search, locals:{tagged_memes: $tagged_memes, sessionid: sessionid})
+    else
+        redirect('/')
+    end       
+end 
+
+# Retrieves tagged memes and redirects to '/vault_search'
+#
+# @see Meme_vault#check_tag
+# @see Meme_vault#search
 post('/searching') do   
     searched_tag = params["Search"]
     if searched_tag.length != 0
@@ -108,13 +169,4 @@ post('/searching') do
         flash[:warning] = "PLEASE ENTER A TAG!"
         redirect('/vault')
     end 
-end 
-
-get('/vault_search') do 
-    if session[:logged_in?] == true
-        sessionid = session[:id] 
-        slim(:vault_search, locals:{tagged_memes: $tagged_memes, sessionid: sessionid})
-    else
-        redirect('/')
-    end       
 end 
--- modell.rb
+++ modell.rb
@@ -2,91 +2,115 @@ require'sqlite3'
 require'byebug'
 require'BCrypt'
 
+module Meme_vault
 
-def connect_to_database
-    db = SQLite3::Database.new("Database/database.db")
-    db.results_as_hash = true
-    return db
-end 
+    #Connects to database
+    #
+    def connect_to_database
+        db = SQLite3::Database.new("Database/database.db")
+        db.results_as_hash = true
+        return db
+    end 
 
-def memes()
-    db = connect_to_database()
-    return db.execute("SELECT * FROM memes ORDER BY MemeId DESC")
-end 
+    # Selects ALL from the memes table
+    #
+    def memes()
+        db = connect_to_database()
+        return db.execute("SELECT * FROM memes ORDER BY MemeId DESC")
+    end 
 
+    # 
+    #
+    def upload_meme(img, imgname, autherid, memetag1, memetag2, memetag3)
+        db = connect_to_database()
+        if imgname.include?(".png") or imgname.include?(".jpg") or imgname.include?(".jpeg")
+            newname = SecureRandom.hex(10) + "." + /(.*)\.(jpg|bmp|png|jpeg)$/.match(imgname)[2]
+            File.open("public/img/#{newname}", 'wb') do |f|
+                f.write(img.read)
+            end
+        end 
+        db.execute("INSERT INTO memes (MemeImgPath, MemeAutherId) VALUES (?,?)", newname, autherid)
+        meme_id = db.last_insert_row_id
+        db.execute("INSERT INTO memes_tags (MemeId, TagId) VALUES (?, ?)", [meme_id, memetag1])
+        db.execute("INSERT INTO memes_tags (MemeId, TagId) VALUES (?, ?)", [meme_id, memetag2])
+        db.execute("INSERT INTO memes_tags (MemeId, TagId) VALUES (?, ?)", [meme_id, memetag3])
+    end 
 
-def encrypt_password(username, password)
-    db = connect_to_database()
-    hashed_password = BCrypt::Password.create(password)
-    db.execute("INSERT INTO users(Username, Password) VALUES(?, ?)", username, hashed_password)
-end 
-
-def login_verification(params_username, params_password) 
-    db = connect_to_database()
-    database_info = db.execute("SELECT Username, Password, UserId FROM users WHERE users.Username = ?", params_username)
-    if database_info.length > 0 && BCrypt::Password.new(database_info.first["Password"]) == params_password
-        return true 
-    else
-        return false
+    # Attempts to delete a row in the memes table
+    #
+    # @param [Integer] memeid, The id of the meme
+    def delete_meme(memeid)
+        db = connect_to_database()
+        db.execute("DELETE FROM memes WHERE MemeId = ?", memeid)
     end
-end 
 
-def select_session_id(params_username)
-    db = connect_to_database()
-    return db.execute("SELECT UserId FROM users WHERE users.Username = ?", params_username)
-end 
+    # Selects ALL from the tags table
+    #
+    def get_tags()
+        db = connect_to_database()
+        return db.execute("SELECT * FROM tags")
+    end
 
-def get_tags()
-    db = connect_to_database()
-    return db.execute("SELECT * FROM tags")
-end
+    # Attempts to insert a new row in the tags table
+    #
+    # @param [String] new_tag, The new tag
+    def add_tag(new_tag)
+        db = connect_to_database()
+        db.execute("INSERT INTO tags (Tag) VALUES (?)", new_tag.downcase)
+    end 
 
-def upload_meme(img, imgname, autherid, memetag1, memetag2, memetag3)
-    db = connect_to_database()
-    if imgname.include?(".png") or imgname.include?(".jpg") or imgname.include?(".jpeg")
-        newname = SecureRandom.hex(10) + "." + /(.*)\.(jpg|bmp|png|jpeg)$/.match(imgname)[2]
-        File.open("public/img/#{newname}", 'wb') do |f|
-            f.write(img.read)
+    # Searches for tag and checks if tag alreadye exists
+    #
+    # param[String] new_tag, The new tag
+    #
+    # @return [true] if tag exists
+    # @return [false] if tag doesn't exist
+    def check_tag(new_tag)
+        db = connect_to_database()
+        if db.execute("SELECT * FROM tags WHERE Tag = ?", new_tag.downcase) != []
+            return true 
+        else 
+            return false 
+        end 
+    end 
+    
+    def encrypt_password(username, password)
+        db = connect_to_database()
+        hashed_password = BCrypt::Password.create(password)
+        db.execute("INSERT INTO users(Username, Password) VALUES(?, ?)", username, hashed_password)
+    end 
+
+    def login_verification(params_username, params_password) 
+        db = connect_to_database()
+        database_info = db.execute("SELECT Username, Password, UserId FROM users WHERE users.Username = ?", params_username)
+        if database_info.length > 0 && BCrypt::Password.new(database_info.first["Password"]) == params_password
+            return true 
+        else
+            return false
         end
     end 
-    db.execute("INSERT INTO memes (MemeImgPath, MemeAutherId) VALUES (?,?)", newname, autherid)
-    meme_id = db.last_insert_row_id
-    db.execute("INSERT INTO memes_tags (MemeId, TagId) VALUES (?, ?)", [meme_id, memetag1])
-    db.execute("INSERT INTO memes_tags (MemeId, TagId) VALUES (?, ?)", [meme_id, memetag2])
-    db.execute("INSERT INTO memes_tags (MemeId, TagId) VALUES (?, ?)", [meme_id, memetag3])
-end 
 
-def add_tag(new_tag)
-    db = connect_to_database()
-    
-    db.execute("INSERT INTO tags (Tag) VALUES (?)", new_tag.downcase)
-end 
-
-def check_tag(new_tag)
-    
-    db = connect_to_database()
-    if db.execute("SELECT * FROM tags WHERE Tag = ?", new_tag.downcase) != []
-        return true 
-    else 
-        return false 
+    def select_session_id(params_username)
+        db = connect_to_database()
+        return db.execute("SELECT UserId FROM users WHERE users.Username = ?", params_username)
     end 
+
+
+
+
+
+    def search(searched_tag)
+        db = connect_to_database()
+        db.results_as_hash = true
+        
+        tag_id = db.execute("SELECT Id FROM tags WHERE Tag =?", searched_tag.downcase) 
+        return db.execute("SELECT * FROM memes INNER JOIN memes_tags ON memes_tags.MemeId = memes.MemeId WHERE TagId =? ORDER BY MemeId DESC", tag_id.first["Id"]) 
+    end 
+
+    def img_path_retriever(tagged_memes)
+        db = connect_to_database()
+        img_paths = db.execute("SELECT MemeImgPath FROM memes WHERE MemeId = ?", )
+    end 
+
+
 end 
-
-def delete_meme(memeid)
-    db = connect_to_database()
-    db.execute("DELETE FROM memes WHERE MemeId = ?", memeid)
-end
-
-def search(searched_tag)
-    db = connect_to_database()
-    db.results_as_hash = true
-    
-    tag_id = db.execute("SELECT Id FROM tags WHERE Tag =?", searched_tag.downcase) 
-    return db.execute("SELECT * FROM memes INNER JOIN memes_tags ON memes_tags.MemeId = memes.MemeId WHERE TagId =? ORDER BY MemeId DESC", tag_id.first["Id"]) 
-end 
-
-def img_path_retriever(tagged_memes)
-    db = connect_to_database()
-    img_paths = db.execute("SELECT MemeImgPath FROM memes WHERE MemeId = ?", )
-end 
-
